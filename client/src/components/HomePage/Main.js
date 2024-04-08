13ebd5ac255d501c4d0e479e094e52e31c527423
Himanshu0422 <himanshumittal035@gmail.com>
2024-04-08 15:14:59 +0530

    main section

---
+++ client/src/components/HomePage/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import backgroundImage from '../../assets/backgroung-image.png';
+import Intro from './Intro';
+
+const Main = () => {
+
+    const backgroundStyles = {
+        backgroundImage: `url(${backgroundImage})`,
+    };
+    return (
+        <div style={backgroundStyles} className='relative h-[100vh] w-[100%] bg-cover bg-center max-sm:flex max-sm:justify-center'>
+            <Intro />
+        </div>
+    )
+}
+
+export default Main;
