f1c4d6867a8de0a8d6a1a831674adf9952445255
Himanshu0422 <himanshumittal035@gmail.com>
2024-04-23 11:40:34 +0530

    installed cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
+const cors = require('cors');
 
 const db = require('./models/index');
 
 const startUpServer = () => {
     const app = express();
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            credentials: true,
+        })
+    );
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
