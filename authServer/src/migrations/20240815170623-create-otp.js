9479e1b9a27532744971ac8c4285e04494eada3d
Himanshu0422 <himanshumittal035@gmail.com>
2024-08-17 01:22:18 +0530

    models and migarations

---
+++ authServer/src/migrations/20240815170623-create-otp.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Otps', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      otp_code: {
+        type: Sequelize.STRING(5),
+        allowNull: false
+      },
+      expires_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      is_used: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Otps');
+  }
+};
---
+++ authServer/src/models/otp.js
@@ -0,0 +1,48 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Otp extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Define the association with the User model
+      Otp.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        onDelete: 'CASCADE'
+      });
+    }
+  }
+  
+  Otp.init({
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+      onDelete: 'CASCADE'
+    },
+    otp_code: {
+      type: DataTypes.STRING(5),
+      allowNull: false
+    },
+    expires_at: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    is_used: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Otp',
+  });
+  
+  return Otp;
+};
