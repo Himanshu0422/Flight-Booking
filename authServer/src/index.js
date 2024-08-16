6f7905538e2a3acbd35cc7360086289c7a938088
Himanshu0422 <himanshumittal035@gmail.com>
2024-08-17 01:21:58 +0530

    authserver setup

---
+++ authServer/src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig');
+const cors = require('cors');
+const colors = require('colors');
+const ApiRoutes = require('./routes/index');
+const passport = require('./config/passport')
+const session = require('express-session'); 
+
+const db = require('./models/index');
+
+const startUpServer = () => {
+    const app = express();
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            credentials: true,
+        })
+    );
+    app.use(session({
+        secret: '123456',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false }
+    }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.use('/api', ApiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`.bgCyan);
+        if(process.env.SYNC_DB === 'true') {
+            db.sequelize.sync({alter: true});
+        }
+    })
+}
+
+startUpServer();
