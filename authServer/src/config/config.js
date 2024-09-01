16615d3e4ab34f49bec49908c61b9e4c41ab52c7
Himanshu0422 <himanshumittal035@gmail.com>
2024-09-01 13:52:34 +0530

    config change

---
+++ authServer/src/config/config.js
@@ -0,0 +1,32 @@
+
+const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_DIALECT, DB_PORT, DB_SSL_CA } = require('./serverConfig');
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "Himanshu@0422",
+    database: "AUTH_DB",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: "Himanshu@0422",
+    database: "AUTH_DB",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
+    port: DB_PORT,
+    dialectOptions: {
+      ssl: {
+        ca: DB_SSL_CA
+      }
+    }
+  }
+};
--- authServer/src/config/serverConfig.js
+++ authServer/src/config/serverConfig.js
@@ -7,5 +7,12 @@ module.exports = {
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     EMAIL_USER:process.env.EMAIL_USER,
     EMAIL_PASS:process.env.EMAIL_PASS,
-    JWT_SECRET:process.env.JWT_SECRET
+    JWT_SECRET:process.env.JWT_SECRET,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_DIALECT: process.env.DB_DIALECT,
+    DB_SSL_CA: process.env.DB_SSL_CA,
 }
