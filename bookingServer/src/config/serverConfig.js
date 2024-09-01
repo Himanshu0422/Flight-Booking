6652f98afb623bde6fb7de7ec5f1b7ddbf338d56
Himanshu0422 <himanshumittal035@gmail.com>
2024-09-01 16:55:52 +0530

    production changes in bookingserver and server

--- bookingServer/src/config/serverConfig.js
+++ bookingServer/src/config/serverConfig.js
@@ -7,7 +7,13 @@ module.exports = {
   FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH,
   RAZORPAY_KEY: process.env.RAZORPAY_KEY,
   RAZORPAY_SECRET: process.env.RAZORPAY_SECRET,
-  EMAIL_USER:process.env.EMAIL_USER,
-  EMAIL_PASS:process.env.EMAIL_PASS,
-  STRIPE_KEY:process.env.STRIPE_KEY,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+  STRIPE_KEY: process.env.STRIPE_KEY,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_DIALECT: process.env.DB_DIALECT
 }
--- server/src/config/serverConfig.js
+++ server/src/config/serverConfig.js
@@ -2,5 +2,11 @@ const dotenv = require('dotenv');
 require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_DIALECT: process.env.DB_DIALECT
 }
