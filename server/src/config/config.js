add88c109e8b31a9773895fabd175654ad2eaca7
Himanshu0422 <himanshumittal035@gmail.com>
2024-09-28 20:56:15 +0530

    getting all data from env

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,20 +1,20 @@
 
-const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_DIALECT, DB_PORT } = require('./serverConfig');
+const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_DIALECT, DB_PORT, DEV_DB_USERNAME, DEV_DB_PASSWORD, DEV_DB_DATABASE, DEV_DB_HOST } = require('./serverConfig');
 
 module.exports = {
   development: {
-    username: "root",
-    password: "Himanshu@0422",
-    database: "Flights_Search_DB",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: DEV_DB_USERNAME,
+    password: DEV_DB_PASSWORD,
+    database: DEV_DB_DATABASE,
+    host: DEV_DB_HOST,
+    dialect: DB_DIALECT,
   },
   test: {
-    username: "root",
-    password: "Himanshu@0422",
-    database: "AUTH_DB",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: DEV_DB_USERNAME,
+    password: DEV_DB_PASSWORD,
+    database: DEV_DB_DATABASE,
+    host: DEV_DB_HOST,
+    dialect: DB_DIALECT,
   },
   production: {
     username: DB_USERNAME,
