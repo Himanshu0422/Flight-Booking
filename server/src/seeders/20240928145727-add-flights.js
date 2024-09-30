27718f0b38d58893660c8c8eb7240d53ba98076d
Himanshu0422 <himanshumittal035@gmail.com>
2024-09-30 07:06:19 +0530

    change in seeder

--- server/src/seeders/20240928145727-add-flights.js
+++ server/src/seeders/20240928145727-add-flights.js
@@ -3,15 +3,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Flights', [
+    const existingFlights = await queryInterface.sequelize.query(
+      `SELECT flightNumber FROM Flights WHERE flightNumber IN ('AI101', 'AI102', 'AI201', 'AI202', 'AI301')`
+    );
+    
+    const existingFlightNumbers = existingFlights[0].map(flight => flight.flightNumber);
+
+    const flightsToInsert = [
       {
         flightNumber: 'AI101',
-        airplaneId: 1, // Assuming this ID exists in the Airplanes table
-        departureAirportId: 1, // Assuming this ID exists in the Airports table
-        arrivalAirportId: 2, // Assuming this ID exists in the Airports table
-        departureTime: '2024-09-30 10:00',
-        arrivalTime: '2024-09-30 12:00',
-        price: 200,
+        airplaneId: 1,
+        departureAirportId: 1,
+        arrivalAirportId: 2,
+        departureTime: '10:00',
+        arrivalTime: '12:00',
+        price: 2000,
         flightTime: '2h',
         nextDay: 0,
         operatingDays: JSON.stringify(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']),
@@ -24,9 +30,9 @@ module.exports = {
         airplaneId: 2,
         departureAirportId: 1,
         arrivalAirportId: 3,
-        departureTime: '2024-09-30 14:00',
-        arrivalTime: '2024-09-30 16:00',
-        price: 250,
+        departureTime: '14:00',
+        arrivalTime: '16:00',
+        price: 2500,
         flightTime: '2h',
         nextDay: 0,
         operatingDays: JSON.stringify(['Saturday', 'Sunday']),
@@ -39,9 +45,9 @@ module.exports = {
         airplaneId: 3,
         departureAirportId: 2,
         arrivalAirportId: 4,
-        departureTime: '2024-09-30 08:00',
-        arrivalTime: '2024-09-30 11:00',
-        price: 350,
+        departureTime: '08:00',
+        arrivalTime: '11:00',
+        price: 3500,
         flightTime: '3h',
         nextDay: 0,
         operatingDays: JSON.stringify(['Monday', 'Wednesday', 'Friday']),
@@ -54,9 +60,9 @@ module.exports = {
         airplaneId: 4,
         departureAirportId: 3,
         arrivalAirportId: 5,
-        departureTime: '2024-09-30 18:00',
-        arrivalTime: '2024-09-30 21:00',
-        price: 400,
+        departureTime: '18:00',
+        arrivalTime: '21:00',
+        price: 4000,
         flightTime: '3h',
         nextDay: 1,
         operatingDays: JSON.stringify(['Tuesday', 'Thursday']),
@@ -69,9 +75,9 @@ module.exports = {
         airplaneId: 5,
         departureAirportId: 4,
         arrivalAirportId: 1,
-        departureTime: '2024-09-30 20:00',
-        arrivalTime: '2024-09-30 23:00',
-        price: 300,
+        departureTime: '20:00',
+        arrivalTime: '23:00',
+        price: 3000,
         flightTime: '3h',
         nextDay: 1,
         operatingDays: JSON.stringify(['Friday', 'Saturday']),
@@ -79,7 +85,14 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       }
-    ], {});
+    ];
+
+    // Filter flights to insert only if they don't already exist
+    const filteredFlights = flightsToInsert.filter(flight => !existingFlightNumbers.includes(flight.flightNumber));
+
+    if (filteredFlights.length > 0) {
+      await queryInterface.bulkInsert('Flights', filteredFlights);
+    }
   },
 
   async down(queryInterface, Sequelize) {
